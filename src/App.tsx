52459d9d788e7f208c333be0feb0c499dd7a2af1
amosdesigns <mail@amosdesigns.net>
2024-04-17 09:59:52 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  
+
+  return (
+    <main className="py-10 h-screen">
+      
+      <h1 className="font-bold text-3xl text-center">Vite + React</h1>
+      
+    </main>
+  );
+}
+
+export default App
